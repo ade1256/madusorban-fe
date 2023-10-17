dd98d5c7b333350b76378e5fc0028c0519576a85
Ade Prasetyo <adeprasetyo174@gmail.com>
2023-10-17 21:49:41 +0700

    fix: import opensans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,35 @@
-import './globals.scss'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.scss";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Madu Sorban | Madu Murni Murah Terpercaya',
-  description: 'Madu murni murah dan dijamin asli terpercaya se-Indonesia',
-}
+  title: "Madu Sorban | Madu Murni Murah Terpercaya",
+  description: "Madu murni murah dan dijamin asli terpercaya se-Indonesia",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/logo.svg" sizes="any" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"anonymous"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
